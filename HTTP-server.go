9d7d5942b5d287daf39764a9f8138b4c3fcf2435
Kazehaya-ctrl <mahitomaki96@gmail.com>
2024-12-29 11:49:22 +0530

    upt: We got a basic server setup in goLang

--- HTTP-server.go
+++ HTTP-server.go
@@ -6,38 +6,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type bodyScheme struct {
-  Name string `json:"name"`
-  Email string `json:"email"`
-}
-
-func requestHandler (router *gin.Engine) {
-  (*router).Any("/", func(ctx *gin.Context) {
-
-    if ctx.Request.Method == "GET" {
-      (*ctx).JSON(http.StatusOK, gin.H{
-        "msg": "Success",
-      })
-    } 
-    if ctx.Request.Method == "POST" {
-      var user bodyScheme
-      if err := ctx.ShouldBindJSON(&user); err != nil  {
-        ctx.JSON(http.StatusBadRequest, gin.H{
-          "msg" : "Failed",
-        })
-        return
-      }
-
-      ctx.JSON(http.StatusOK, gin.H{
-        "msg": "Sucess",
-        "data": user,
-      })
+func main() {
+  router := gin.Default()
+  router.LoadHTMLGlob("./index.html")
+  router.GET("/", func(ctx *gin.Context) {
+    // data, err := os.ReadFile("index.html")
+    // if err != nil {
+    //   return
+    // }
+    data2 := gin.H{
+      "msg" : "Sucess",
     }
+    ctx.HTML(http.StatusOK, "index.html", data2)
   })
-}
-
-func main() { 
-  r := gin.Default()
-  requestHandler(r)
-  r.Run()
+  // fmt.Println(string(data))
+  router.Run()
 }
